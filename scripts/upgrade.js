419d5a0be54f95e2a23069a16e964de6ff8f0fa2
Marco <matabasco@gmail.com>
2023-03-10 01:13:42 +0100

    link2

--- scripts/upgrade.js
+++ scripts/upgrade.js
@@ -3,10 +3,10 @@ const { ethers, upgrades } = require('hardhat');
 async function upgradeSSVNetwork() {
 
   //const Lock2 = await ethers.getContractFactory("Lock2");
-  const Lock = await ethers.getContractFactory("Lock");
+  const Lock2 = await ethers.getContractFactory("Lock2");
 
   //await upgrades.forceImport('0x81d9Ff669e40b4237B5e3Ced62b71a6f08665f3d', Lock, { kind: 'uups' });
-  await upgrades.upgradeProxy('0x094025275548bEb649D02255130587D972968815', Lock, { kind: 'uups' });
+  await upgrades.upgradeProxy('0x4c115F5948Ba632fF9B0a5a40f84Af9A825484e0', Lock2, { kind: 'uups' });
 
   console.log("Lock upgraded successfully");
 }
